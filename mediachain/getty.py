f41298d83f3de1214c1894b8e90d675684a72387
Yusef Napora <yusef@napora.org>
2016-05-26 14:39:49 -0400

    simple getty json to artefact fn

---
+++ mediachain/getty.py
@@ -0,0 +1,41 @@
+import json
+
+from os import walk
+from os.path import join
+
+from data_objects import Artefact
+from dateutil import parser as date_parser
+
+
+def getty_to_artefact(getty_json):
+    meta = {'_id': 'getty_' + getty_json['id'],
+            'title': getty_json['title'],
+            'artist': getty_json['artist'],
+            'collection_name': getty_json['collection_name'],
+            'caption': getty_json['caption'],
+            'editorial_source': getty_json['editorial_source'].get('name', None),
+            'keywords': ' '.join(
+                [x['text'] for x in getty_json['keywords'] if 'text' in x]),
+            'date_created': date_parser.parse(getty_json['date_created'])
+            }
+
+    Artefact(meta)
+
+
+def getty_artefacts(dd='getty/json/images',
+                    max_num=0):
+    nn = 0
+    for dir_name, subdir_list, file_list in walk(dd):
+        for fn in file_list:
+            nn += 1
+
+            if max_num and (nn + 1 >= max_num):
+                print ('ENDING EARLY')
+                return
+
+            fn = join(dir_name, fn)
+
+            with open(fn) as f:
+                getty = json.load(f)
+
+            yield getty_to_artefact(getty)
