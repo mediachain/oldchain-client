9ec17a8cd65ea4da9ff2eef1492863b7f10dba1a
Yusef Napora <yusef@napora.org>
2016-05-31 12:28:29 -0400

    add grpc transactor client

---
+++ mediachain/transactor.py
@@ -0,0 +1,35 @@
+from grpc.beta import implementations
+from mediachain.proto import Transactor_pb2
+from mediachain.data_objects import Artefact, Entity, ChainCell, \
+    MultihashReference
+
+TIMEOUT_SECS = 120
+
+
+def assert_canonical(record):
+    assert(isinstance(record, Artefact) or isinstance(record, Entity),
+           "Expected artefact or entity, got " + str(type(record)))
+
+
+def assert_chaincell(cell):
+    assert(isinstance(cell, ChainCell),
+           "Expected chain cell, got " + str(type(cell)))
+
+
+class TransactorClient(object):
+
+    def __init__(self, host, port):
+        channel = implementations.insecure_channel(host, port)
+        self.client = Transactor_pb2.beta_create_TransactorService_stub(channel)
+
+    def insert(self, record):
+        assert_canonical(record)
+        req = Transactor_pb2.InsertRequest(record.to_cbor_bytes())
+        ref = self.client.InsertCanonicalRecord(req)
+        return MultihashReference.from_base58(ref.multihash)
+
+    def update(self, cell):
+        assert_chaincell(cell)
+        req = Transactor_pb2.UpdateRequest(cell.to_cbor_bytes())
+        ref = self.client.UpdateCanonicalRecord(req)
+        return MultihashReference.from_base58(ref.multihash)
