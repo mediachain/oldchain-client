27c3972a57e09faf35f86b82b35eb815dadc4688
Cole Brown <bigswim@gmail.com>
2016-06-01 14:21:40 -0400

    Make get_object pull appropriate fields
    Temporarily set up dynamo to work internally

--- mediachain/reader/dynamo.py
+++ mediachain/reader/dynamo.py
@@ -1,15 +1,20 @@
 import boto3
+import cbor
 
 def get_table(name):
-    dynamo = boto3.resource('dynamo')
+    dynamo = boto3.resource('dynamodb',
+                            endpoint_url='http://localhost:8000',
+                            region_name='us-east-1',
+                            aws_access_key_id='',
+                            aws_secret_access_key='')
     return dynamo.Table(name)
 
 def get_object(reference):
-    table = get_table('mediachain')
+    table = get_table('Mediachain')
     obj = table.get_item(Key={'multihash': reference})
-    byte_string = obj['Item']['data']
 
-    if byte_string is None:
+    if obj is None:
         raise KeyError('Could not find key <%s> in Dynamo'.format(reference))
 
+    byte_string = obj['Item']['data'].value
     return cbor.loads(byte_string)
