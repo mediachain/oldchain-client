dc811d51ec1cc70e0ebbe1834065ea4e1544cf4f
Yusef Napora <yusef@napora.org>
2016-06-17 11:16:30 -0400

    fill in getty translator implementation

--- mediachain/translation/getty/translator.py
+++ mediachain/translation/getty/translator.py
@@ -1,4 +1,5 @@
 import os
+import json
 from mediachain.translation.translator import Translator
 
 
@@ -10,7 +11,45 @@ class GettyTranslator(Translator):
 
     @staticmethod
     def translate(raw_metadata):
-        return {}
+        getty_json = json.loads(raw_metadata, 'utf-8')
+
+        # extract artist Entity
+        artist_name = getty_json['artist']
+        artist_entity = {
+            'type': 'entity',
+            'meta': {
+                'data': {
+                    'name': artist_name
+                }
+            }
+        }
+
+        # extract artwork Artefact
+        data = {u'_id': u'getty_' + getty_json['id'],
+                u'title': getty_json['title'],
+                u'artist': getty_json['artist'],
+                u'collection_name': getty_json['collection_name'],
+                u'caption': getty_json['caption'],
+                u'editorial_source':
+                    getty_json['editorial_source'].get('name', None),
+                u'keywords':
+                    [x['text'] for x in getty_json['keywords'] if 'text' in x],
+                u'date_created': getty_json['date_created']
+                }
+
+        artwork_artefact = {
+            'type': 'artefact',
+            'meta': {'data': data}
+        }
+
+        return {
+            'object': artwork_artefact,
+            'related': {
+                'artefactCreatedBy': {
+                    'object': artist_entity
+                }
+            }
+        }
 
     @staticmethod
     def can_translate_file(file_path):
