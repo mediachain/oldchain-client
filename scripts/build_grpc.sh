ab6e7646d04d3a28173aaa564990ec5601d36687
Arkadiy Kukarkin <arkadiy@mine.nyc>
2016-06-27 18:10:44 -0400

    Smarter subtree pull

--- scripts/build_grpc.sh
+++ scripts/build_grpc.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 REMOTE=protobuf
-PROTOBUF_DIR=$(dirname $0)/../protobuf
-PROTOS_OUT_DIR=$(dirname $0)/../mediachain/proto
+BASEDIR=$(dirname $0)/..
+PROTOBUF_DIR=$BASEDIR/protobuf
+PROTOS_OUT_DIR=$BASEDIR/mediachain/proto
 VERSION="0.0.1"
 #FILES=("Transactor.proto")
 
 mkdir -p $PROTOS_OUT_DIR
-rm $PROTOS_OUT_DIR/[!_]*
+rm -f $PROTOS_OUT_DIR/[!_]*
 
 echo "Updating protocol protobuf from upstream ($VERSION)"
 
-git subtree pull -P $PROTOBUF_DIR $REMOTE $VERSION
+git -C $BASEDIR subtree pull --squash -P protobuf $REMOTE $VERSION -m "Updated protobuf to $VERSION"
 
 if [ $? -eq 0 ]
 then
