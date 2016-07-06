4f3a764f85fd19d1489f57f7629901db857e21bd
Arkadiy Kukarkin <arkadiy@mine.nyc>
2016-07-06 18:31:29 -0400

    Check protobuf regen exit code

--- scripts/build_grpc.sh
+++ scripts/build_grpc.sh
@@ -11,8 +11,19 @@ rm -f $PROTOS_OUT_DIR/[!_]*
 
 echo "Regenerating protobuf..."
 
+if [ ! -d $PROTOBUF_DIR ]
+then
+    echo "Could not find protobuf sources, distribution incomplete"
+    exit 1
+fi
+
 for file in `ls $PROTOBUF_DIR`; do
     $PYTHON -m grpc.tools.protoc -I $PROTOBUF_DIR  --python_out=$PROTOS_OUT_DIR --grpc_python_out=$PROTOS_OUT_DIR $PROTOBUF_DIR/$file
+    if [ $? -ne 0 ]
+    then
+        echo "Failed to generate protobuf for $file!"
+        exit $?
+    fi
 done
 
 echo "Done!"
