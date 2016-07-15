14d22ee95e680e9e96d323030973877f1b49f28d
Arkadiy Kukarkin <arkadiy@mindhole.org>
2016-07-15 16:28:09 -0400

    Remove old translator lookup mechanism

--- mediachain/translation/lookup.py
+++ mediachain/translation/lookup.py
@@ -1,14 +1,4 @@
-from getty.translator import GettyTranslator
-
-_TRANSLATORS = {
-    GettyTranslator.translator_id(): GettyTranslator
-}
-
 
 def get_translator(translator_id):
-    try:
-        return _TRANSLATORS[translator_id]
-    except LookupError:
-        raise LookupError(
-            "No translator with id {}".format(translator_id)
-        )
+    raise NotImplementedError
+
