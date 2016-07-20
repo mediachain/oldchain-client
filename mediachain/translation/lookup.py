a77349df4b80e4b3cd037c518fc9c69f784072a0
Yusef Napora <yusef@napora.org>
2016-07-20 12:25:03 -0400

    only fetch translator from ipfs if it doesn't exist locally from prev run

--- mediachain/translation/lookup.py
+++ mediachain/translation/lookup.py
@@ -3,6 +3,7 @@ import sys
 import os
 from os.path import expanduser, join
 
+
 class ChDir(object):
     """
     Step into a directory temporarily
@@ -17,28 +18,36 @@ class ChDir(object):
     def __exit__(self, *args):
         os.chdir(self.old_dir)
 
+
 def get_translator(translator_id):
-	try:
-		name, version = translator_id.split('@')
-	except ValueError:
-		raise LookupError(
-			"Bad translator id `{}`, must be `name@multihash` format".format(translator_id)
-		)
+    try:
+        name, version = translator_id.split('@')
+    except ValueError:
+        raise LookupError(
+            "Bad translator id `{}`, must be `name@multihash` format".format(translator_id)
+        )
 
-	ipfs = get_ipfs_datastore() # FIXME: memoize this
+    path = join(expanduser('~'), '.mediachain')
+    if not os.path.exists(path):
+        os.makedirs(path)
 
-	path = join(expanduser('~'), '.mediachain')
-	if not os.path.exists(path):
-	    os.makedirs(path)
+    try:
+        return load_translator(path, name, version)
+    except ImportError:
+        pass
 
-	with ChDir(path):
-		translator = ipfs.client.get(version) # FIXME: timeout, error handling
+    ipfs = get_ipfs_datastore() # FIXME: memoize this
+    with ChDir(path):
+        translator = ipfs.client.get(version) # FIXME: timeout, error handling
 
-	sys.path.append(path)
-	# print('dynamic module load path: {}'.format(path))
-	full_path = version + '.translator'
-	# print('loading translator module from {}'.format(full_path))
-	translator_module = __import__(full_path, globals(), locals(), [name])
-	translator = getattr(translator_module, name.capitalize())
+    return load_translator(path, name, version)
 
-	return translator
+
+def load_translator(base_path, name, version):
+    if base_path not in sys.path:
+        sys.path.append(base_path)
+
+    module_path = version + '.translator'
+    translator_module = __import__(module_path, globals(), locals(), [name])
+    translator = getattr(translator_module, name.capitalize())
+    return translator
