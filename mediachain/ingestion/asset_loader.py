163b1927d4878a3cbc16f2090bd0b376d45e5b6b
Yusef Napora <yusef@napora.org>
2016-07-22 14:52:03 -0400

    write binary asset links with ipld-style link properties

--- mediachain/ingestion/asset_loader.py
+++ mediachain/ingestion/asset_loader.py
@@ -2,6 +2,7 @@ import os
 from urlparse import urlparse
 import requests
 import base64
+import mimetypes
 from PIL import Image
 from StringIO import StringIO
 
@@ -10,27 +11,30 @@ def load_asset(asset):
     try:
         uri = asset['uri']
     except (TypeError, KeyError):
-        return None
+        return None, None
+
+    mime, _ = mimetypes.guess_type(uri)
 
     if uri.lower().startswith('file://'):
         p = urlparse(uri)
         file_path = os.path.abspath(os.path.join(p.netloc, p.path))
+
         if not os.path.isfile(file_path):
-            return None
+            return None, None
         try:
             with open(file_path) as f:
-                return f.read()
+                return f.read(), mime
         except IOError as e:
             print('error reading from {}: {}'.format(file_path, e))
-            return
+            return None, None
 
     try:
         r = requests.get(uri)
-        return r.content
+        return r.content, mime
     except requests.exceptions.RequestException as e:
         print('error downloading media from {}: {}'.format(uri, e))
 
-    return None
+    return None, None
 
 
 def process_asset(name, asset_data):
--- mediachain/reader/api.py
+++ mediachain/reader/api.py
@@ -1,17 +1,14 @@
-from mediachain.datastore import get_db, get_raw_datastore
-from mediachain.ingestion.asset_loader import make_jpeg_data_uri
+from mediachain.datastore import get_db
 import copy
 import base58
-import json
-from pygments import highlight
 from utils import dump
 
 def get_and_print_object(transactor, object_id):
-    obj = get_object(transactor, object_id, fetch_images=False)
+    obj = get_object(transactor, object_id)
     dump(obj)
 
 
-def get_object(transactor, object_id, fetch_images=True):
+def get_object(transactor, object_id):
     db = get_db()
     base = db.get(object_id)
     head = transactor.get_chain_head(object_id)
@@ -20,40 +17,11 @@ def get_object(transactor, object_id, fetch_images=True):
 
     try:
         entity_id = obj['entity']
-        obj['entity'] = get_object(transactor, entity_id, fetch_images)
+        obj['entity'] = get_object(transactor, entity_id)
     except KeyError as e:
         pass
 
-    if fetch_images and obj.get('type') == 'artefact':
-        return fetch_thumbnails(obj)
     return obj
-
-
-def fetch_thumbnails(obj):
-    def with_fallback():
-        o = copy.deepcopy(obj)
-        if 'meta' not in o:
-            o['meta'] = {}
-        if 'data' not in o['meta']:
-            o['meta']['data'] = {}
-
-        o['meta']['data']['thumbnail_base64'] = 'NO_IMAGE'
-        return o
-
-    try:
-        thumb_ref_bytes = obj['meta']['data']['thumbnail']['@link']
-        thumb_ref = base58.b58encode(thumb_ref_bytes)
-        db = get_raw_datastore()
-        thumb = db.get(thumb_ref)
-    except (ValueError, LookupError):
-        return with_fallback()
-
-    with open('/tmp/thumbnail.jpg', 'wb') as f:
-        f.write(thumb)
-
-    with_thumb = copy.deepcopy(obj)
-    with_thumb['meta']['data']['thumbnail_base64'] = make_jpeg_data_uri(thumb)
-    return with_thumb
 
 
 def apply_update_cell(acc, cell):
