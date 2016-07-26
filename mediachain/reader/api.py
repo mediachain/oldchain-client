257f225fe974281fb1b66b5ba2e45c6355544a1b
Yusef Napora <yusef@napora.org>
2016-07-26 17:36:49 -0400

    use recursive dict merge when applying update cells

--- mediachain/reader/api.py
+++ mediachain/reader/api.py
@@ -74,12 +74,24 @@ def fetch_thumbnails(obj):
     return with_thumb
 
 
+def merge(a, b, path=None):
+    "merges b into a"
+    if path is None: path = []
+    for key in b:
+        if key in a:
+            if isinstance(a[key], dict) and isinstance(b[key], dict):
+                merge(a[key], b[key], path + [str(key)])
+            else:
+                a[key] = b[key]
+        else:
+            a[key] = b[key]
+    return a
+
 def apply_update_cell(acc, cell):
     result = copy.deepcopy(acc)
     cell = copy.deepcopy(cell)
 
-    for k, v in cell['meta'].iteritems():
-        result['meta'][k] = v
+    merge(result['meta'], cell['meta'])
 
     return result
 
