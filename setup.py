ddbef604775f1c95f99ed2fa3f3b21b9e48e5614
Yusef Napora <yusef@napora.org>
2016-08-08 14:12:51 -0400

    bump version to 0.1.11

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ class build_py(_build_py):
         _build_py.run(self)
 
 setup(
-    version='0.1.10',
+    version='0.1.11',
     name='mediachain-client',
     description='mediachain reader command line interface',
     author='Mediachain Labs',
